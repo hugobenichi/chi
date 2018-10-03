998f323dda6cf681675fd4f33eb3e06d6417015f
Hugo Benichi <hugobenichi@gmail.com>
2018-10-03 23:45:29 +0000

    term: better error handling + TODOs implemented + slice bootstrap

--- chi.c
+++ chi.c
@@ -38,8 +38,8 @@ typedef uint64_t  u64;
 #define __LOC__ __FILE__ ":" __stringize2(__LINE__)
 
 // Careful: the result expr gets evaluated twice
-#define __max(x,y) ((x) > (y) ? : (x) : (y))
-#define __min(x,y) ((x) < (y) ? : (x) : (y))
+#define max(x,y) ((x) > (y) ? (x) : (y))
+#define min(x,y) ((x) < (y) ? (x) : (y))
 
 // Print formatted error msg and exit.
 void fatal(const char * format, ...)
@@ -51,14 +51,60 @@ void fatal(const char * format, ...)
         fprintf(stderr, "\n");
         exit(1);
 }
+#define __fatal(format, ...) fatal(__LOC__ " %s [ " format, __func__, ##__VA_ARGS__)
 
 // Check if condition is false or true respectively, otherwise print formatted message and exit.
 #define __fail_if2(condition, format, ...) if (condition) fatal(__LOC__ " %s [ " format, __func__, ##__VA_ARGS__)
 #define __assert2(condition, format, ...) if (!(condition)) fatal(__LOC__ " %s [ " format, __func__, ##__VA_ARGS__)
 // Same, but print the failed condition instead of a formatted message.
 #define __assert1(condition) __assert2(condition, "assert failed: \"" __stringize2(condition) "\"")
-#define __fail_if1(condition) __fail_if2(condition, "fatal condition: \"" __stringize2(condition) "\"")
+#define __fail_if(condition) __fail_if2(condition, "fatal condition: \"" __stringize2(condition) "\"")
+// TODO: replace strerror with just a mapping of errno values to errno symbols like ENOPERM
+#define __efail_if(condition) __fail_if2(condition, "fatal condition: \"" __stringize2(condition) "\" failed with: %s", strerror(errno))
 
+
+/**********
+ * MEMORY *
+ **********/
+
+struct slice {
+	u8 *start;
+	u8 *stop;
+};
+
+#define slice_wrap_string(string) s(string, string + sizeof(string) - 1)
+
+struct slice s(u8 *start, u8 *stop)
+{
+	return (struct slice) {
+		.start = start,
+		.stop = stop,
+	};
+}
+
+size_t slice_len(struct slice s)
+{
+	return s.stop - s.start;
+}
+
+struct slice slice_copy(struct slice dst, const struct slice src)
+{
+	size_t dst_len = slice_len(dst);
+	size_t src_len = slice_len(src);
+	size_t len = min(dst_len, src_len);
+	memcpy(dst.start, src.start, len);
+	return s(dst.start + len, dst.stop);
+}
+
+struct buffer {
+	char *memory;
+	size_t size;
+};
+
+struct slice buffer_to_slice(struct buffer b)
+{
+	return s(b.memory, b.memory + b.size);
+}
 
 /***********
  * LOGGING *
@@ -121,24 +167,12 @@ int log_formatted_message(const char *format, ...)
 #include <errno.h>
 #include <termios.h>
 
-// CLEANUP: inline term constants !
-// TODO: make these consistent with other termraw functions in github.com/hugobenichi/czl
+static const char term_seq_finish[]                       = "\x1b[0m";
+static const char term_clear[]                            = "\x1bc";
+static const char term_newline[]                          = "\r\n";
+static const char term_cursor_hide[]                      = "\x1b[?25l";
+static const char term_cursor_show[]                      = "\x1b[?25h";
 
-//static const char term_seq_finish[]                       = "\x1b[0m";
-//static const char term_clear[]                            = "\x1bc";
-//static const char term_newline[]                          = "\r\n";
-//static const char term_cursor_hide[]                      = "\x1b[?25l";
-//static const char term_cursor_show[]                      = "\x1b[?25h";
-static const char term_cursor_save[]                      = "\x1b[s";
-static const char term_cursor_restore[]                   = "\x1b[u";
-static const char term_switch_offscreen[]                 = "\x1b[?47h";
-static const char term_switch_mainscreen[]                = "\x1b[?47l";
-static const char term_switch_mouse_event_on[]            = "\x1b[?1000h";
-static const char term_switch_mouse_tracking_on[]         = "\x1b[?1002h";
-static const char term_switch_mouse_tracking_off[]        = "\x1b[?1002l";
-static const char term_switch_mouse_event_off[]           = "\x1b[?1000l";
-//static const char term_switch_focus_event_on[]            = "\x1b[?1004h";
-//static const char term_switch_focus_event_off[]           = "\x1b[?1004l";
 
 vec term_get_size()
 {
@@ -152,59 +186,73 @@ vec term_get_size()
 	return v(w.ws_row, w.ws_col);
 }
 
-struct termios termios_initial = {};
+static struct termios termios_initial = {};
 
 void term_restore()
 {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_initial);
-	//fprintf(stdout, term_switch_focus_event_off);
-	fprintf(stdout, term_switch_mouse_tracking_off);
-	fprintf(stdout, term_switch_mouse_event_off);
-	fprintf(stdout, term_switch_mainscreen);
-	fprintf(stdout, term_cursor_restore);
+	__efail_if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_initial));
+	__efail_if(fprintf(stdout, "\x1b[?1004l"));
+	__efail_if(fprintf(stdout, "\x1b[?1002l"));
+	__efail_if(fprintf(stdout, "\x1b[?1000l"));
+	__efail_if(fprintf(stdout, "\x1b[?47l"));
+	__efail_if(fprintf(stdout, "\x1b[u"));
+	__efail_if(fflush(stdout));
 }
+
 
 void term_raw()
 {
 	int z;
-	z = tcgetattr(STDIN_FILENO, &termios_initial);
-	if (z < 0) {
-		// TODO: use fail function
-		errno = ENOTTY;
-		perror("term_raw: tcgetattr() failed");
-		exit(1);
-	}
+
+	__efail_if(tcgetattr(STDIN_FILENO, &termios_initial));
 	atexit(term_restore);
 
-	fprintf(stdout, term_cursor_save);
-	fprintf(stdout, term_switch_offscreen);
-	fprintf(stdout, term_switch_mouse_event_on);
-	fprintf(stdout, term_switch_mouse_tracking_on);
-	//fprintf(stdout, term_switch_focus_event_on); // TODO: turn on and check if files need reload.
+	__efail_if(fprintf(stdout, "\x1b[s"));
+	__efail_if(fprintf(stdout, "\x1b[?47h"));
+	__efail_if(fprintf(stdout, "\x1b[?1000h"));
+	__efail_if(fprintf(stdout, "\x1b[?1002h"));
+	__efail_if(fprintf(stdout, "\x1b[?1004h"));
+	__efail_if(fflush(stdout));
 
 	struct termios termios_raw = termios_initial;
-	termios_raw.c_iflag &= ~BRKINT;                    // no break
-	termios_raw.c_iflag &= ~ICRNL;                     // no CR to NL
-	termios_raw.c_iflag &= ~INPCK;                     // no parity check
-	termios_raw.c_iflag &= ~ISTRIP;                    // no strip character
-	termios_raw.c_iflag &= ~IXON;                      // no CR to NL
-	termios_raw.c_oflag &= ~OPOST;                     // no post processing
-	termios_raw.c_lflag &= ~ECHO;                      // no echo
-	termios_raw.c_lflag &= ~ICANON;
-	termios_raw.c_lflag &= ~ISIG;
-	termios_raw.c_cc[VMIN] = 0;                        // return each byte, or nothing when timeout
-	termios_raw.c_cc[VTIME] = 100;                     // 100 * 100 ms timeout
+	// Input modes
+	termios_raw.c_iflag &= ~INPCK;                  // no parity check
+	termios_raw.c_iflag &= ~PARMRK;			// no parity check
+	termios_raw.c_iflag &= ~ISTRIP;                 // no strip character
+	termios_raw.c_iflag &= ~IGNBRK;			// do not ignore break conditions
+	termios_raw.c_iflag &= ~BRKINT;                 // no break
+	termios_raw.c_iflag &= ~IGNCR; 			// keep CR on RET key
+	termios_raw.c_iflag &= ~ICRNL;                  // no CR to NL
+	termios_raw.c_iflag &= ~INLCR; 			// no NL to CR conversion
+	termios_raw.c_iflag &= ~IXON;                   // no CR to NL
+	termios_raw.c_iflag &= ~IXOFF; 			// no start/stop control chars on input
+	// Output modes
+	termios_raw.c_oflag &= ~OPOST;			// no post processing
+	// Local modes
+	termios_raw.c_lflag &= ~ECHO;			// no echo
+	termios_raw.c_lflag &= ~ECHONL;			// no NL echo
+	termios_raw.c_lflag &= ~ICANON;			// no canonical mode, read input as soon as available
+	termios_raw.c_lflag &= ~IEXTEN;			// no extension
+	termios_raw.c_lflag &= ~ISIG;			// turn off sigint, sigquit, sigsusp
+	termios_raw.c_cc[VMIN] = 0;			// return each byte, or nothing when timeout
+	termios_raw.c_cc[VTIME] = 100;			// 100 * 100 ms timeout
 	termios_raw.c_cflag |= CS8;                        // 8 bits chars
-
-	z = tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_raw);
-	if (z < 0) {
-		errno = ENOTTY;
-		perror("term_raw: tcsetattr() failed");
-		exit(1);
-	}
+	__efail_if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_raw));
 }
 
+struct term_frame {
+	// TODO
+};
 
+void term_render(struct buffer buffer, struct term_frame *frame)
+{
+	struct slice slice = buffer_to_slice(buffer);
+
+	// How to use slice for copying immutable const char* into a mutable slice ?
+	slice = slice_copy(slice, slice_wrap_string(term_clear));
+	slice = slice_copy(slice, slice_wrap_string(term_cursor_hide));
+	slice = slice_copy(slice, slice_wrap_string(term_cursor_show));
+}
 
 
 /********
@@ -215,15 +263,15 @@ void term_raw()
 int main(int argc, char **args)
 {
 	logm("enter");
+
+	term_raw();
+
 	for (int i = 0; i < argc; i++) {
 		puts(args[i]);
+		puts("\n");
 		logm("this is a log:%s", args[i]);
 		logs(args[i]);
 	}
 
 	logm("exit");
-
-
-	//__assert1(2 < 1);
-	__fail_if1(1 < 4);
 }
