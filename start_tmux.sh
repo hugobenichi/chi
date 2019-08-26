ed38390eeb523acef9d506f28f9096cbee280875
Hugo Benichi <hugobenichi@gmail.com>
2019-08-27 08:50:01 +0900

    add tmux script

---
+++ start_tmux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#set -eux
+
+SESSION=chi
+EDITOR=vim
+LOGS=/tmp/ciseau.log
+
+tmux kill-session -t $SESSION || echo "no previous session"
+
+tmux new-session -P -d -s $SESSION -n 'Editor' $EDITOR src/
+#tmux send-keys ':vs' C-m
+
+tmux new-window -P -n 'Build'
+#tmux send-keys '. Build' C-m
+
+[ - $LOGS ] || touch $LOGS
+
+tmux new-window -P -n 'Log'
+tmux send-keys "tail -f " $LOGS C-m
+
+tmux select-window -t $SESSION:1
+tmux -2 attach-session -t $SESSION
